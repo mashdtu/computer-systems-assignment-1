2ea066d73c8936e0af6d9f54fcc156f5bf020ded
mash <mathiashk8d@gmail.com>
2025-09-29 19:38:49 +0200

    wrote eroding function

--- main.c
+++ main.c
@@ -70,14 +70,97 @@ void binaryToRGB (unsigned char binary_image[BMP_WIDTH][BMP_HEIGTH], unsigned ch
 }
 
 
-void erode;
+// Apply the erosion algorithm to the binary image using a structuring element.
+void erode (unsigned char binary_image[BMP_WIDTH][BMP_HEIGTH]) {
+
+    // Define a 3x3 structuring element (cross shape for cell detection).
+    // 1 means the pixel is part of the structuring element, 0 means it's ignored.
+
+    // Define size of structuring element.
+    int se_size = 3;
+
+    // Define center of the structuring element.
+    int se_center = se_size / 2; // Used for making sure the pixel is not at the border. Dividing integers automatically rounds down.
+
+    // Define the structuring element itself.
+    int structuringElement[3][3] = {
+        {0, 1, 0},
+        {1, 1, 1},
+        {0, 1, 0}
+    };
+    
+    // Create a temporary array to store the result.
+    unsigned char temp_image[BMP_WIDTH][BMP_HEIGTH];
+    
+    // Copy the original binary image to the temporary array.
+    for (int x = 0; x < BMP_WIDTH; x++) {
+        for (int y = 0; y < BMP_HEIGTH; y++) {
+            temp_image[x][y] = binary_image[x][y];
+        }
+    }
+    
+    // Apply the erosion algorithm for non-border pixels
+    for (int x = se_center; x < BMP_WIDTH - se_center; x++) {
+        for (int y = se_center; y < BMP_HEIGTH - se_center; y++) {
+            
+            // Check if the selected pixel should be eroding by comparing the surrounding grid with the structuring element.
+
+            // Assume erosion passes initially, i.e. that there are no black pixels within the structuring element.
+            int erosion_result = 1;
+            
+            // Check each position in the structuring element projected on the binary image.
+            for (int i = 0; i < se_size; i++) {
+                for (int j = 0; j < se_size; j++) {
+
+                    // Only check positions where the structuring element has a 1.
+                    if (structuringElement[i][j] == 1) {
+
+                        // Calculate the position of the selected entry of the structuring element relative to the selected pixel.
+                        int entry_x = x + (i - se_center);
+                        int entry_y = y + (j - se_center);
+                        
+                        // If any black pixel is contain on an entry of the structuring element equal to 1, the erosion fails.
+                        if (temp_image[entry_x][entry_y] == 0) {
+                            erosion_result = 0;
+                            break;
+                        }
+                    }
+                }
+
+                // Skip the loop if the erosion already fails.
+                if (erosion_result == 0) break;
+            }
+            
+            // Set the pixel to the result of the erosion, i.e. if the erosion failed (0), then set the pixel to 0 (black). If it succeeded set it to 1 (white).
+            binary_image[x][y] = erosion_result;
+        }
+    }
+    
+    // Set border pixels to black to avoid boundary issues.
+
+    // Horizontal borders:
+    for (int x = 0; x < BMP_WIDTH; x++) {
+        for (int i = 0; i < se_center; i++) {
+            binary_image[x][i] = 0;  // Top border
+            binary_image[x][BMP_HEIGTH - 1 - i] = 0;  // Bottom border
+        }
+    }
+
+    // Vertical borders:
+    for (int y = 0; y < BMP_HEIGTH; y++) {
+        for (int i = 0; i < se_center; i++) {
+            binary_image[i][y] = 0;  // Left border
+            binary_image[BMP_WIDTH - 1 - i][y] = 0;  // Right border
+        }
+    }
+}
 
 
 
 
 
 
-void detect;
+// void detect;
 
 
 
@@ -95,31 +178,38 @@ unsigned char binary_image[BMP_WIDTH][BMP_HEIGTH];
 // Main function.
 int main(int argc, char** argv)
 {
-  //argc counts how may arguments are passed
-  //argv[0] is a string with the name of the program
-  //argv[1] is the first command line argument (input image)
-  //argv[2] is the second command line argument (output image)
+    //argc counts how may arguments are passed
+    //argv[0] is a string with the name of the program
+    //argv[1] is the first command line argument (input image)
+    //argv[2] is the second command line argument (output image)
 
-  //Checking that 2 arguments are passed
-  if (argc != 3)
-  {
-      fprintf(stderr, "Usage: %s <output file path> <output file path>\n", argv[0]);
-      exit(1);
-  }
+    //Checking that 2 arguments are passed
+    if (argc != 4) {
+        fprintf(stderr, "Usage: %s <output file path> <output file path>\n", argv[0]);
+        exit(1);
+    }
 
-  printf("Example program - 02132 - A1\n");
+    printf("Example program - 02132 - A1\n");
 
-  // Load image from file.
-  read_bitmap(argv[1], input_image);
+    // Load image from file.
+    read_bitmap(argv[1], input_image);
 
-  // Write the binary image.
-  rgbToBinary(input_image, binary_image);
+    // Write the binary image.
+    rgbToBinary(input_image, binary_image);
 
-  binaryToRGB(binary_image, output_image);
+    binaryToRGB(binary_image, output_image);
 
-  //Save image to file
-  write_bitmap(output_image, argv[2]);
+    // Save image to file
+    write_bitmap(output_image, argv[2]);
 
-  printf("Done!\n");
-  return 0;
+    // erode image
+    //erode(binary_image);
+
+    //binaryToRGB(binary_image, output_image);
+
+    // Save image to file
+    //write_bitmap(output_image, argv[3]);
+
+    printf("Done!\n");
+    return 0;
 }
