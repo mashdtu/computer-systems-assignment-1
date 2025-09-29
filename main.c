ff21d9ed18b1f67c300e329cee4fafa7b0e4d2de
mash <mathiashk8d@gmail.com>
2025-09-29 16:44:23 +0200

    better documentation

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
-//To compile (linux/mac): gcc cbmp.c main.c -o main.out -std=c99
-//To run (linux/mac): ./main.out example.bmp example_gray.bmp
+// To compile (linux/mac): gcc cbmp.c main.c -o main.out -std=c99
+// To run (linux/mac): ./main.out example.bmp example_gray.bmp
 
-//To compile (win): gcc cbmp.c main.c -o main.exe -std=c99
-//To run (win): main.exe example.bmp example_inv.bmp
+// To compile (win): gcc cbmp.c main.c -o main.exe -std=c99
+// gcc main.c -o main.exe
+// To run (win): main.exe example.bmp example_inv.bmp
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -20,19 +21,19 @@ void forceBlack(unsigned char binary_image[BMP_WIDTH][BMP_HEIGTH], unsigned int 
 
 // Get pixel colour.
 int getColour(unsigned char binary_image[BMP_WIDTH][BMP_HEIGTH], unsigned int x, unsigned int y) {
-    // Return 1 if pixel is white and 0 if the pixel is black
+    // Return 1 if pixel is white and 0 if the pixel is black.
     return binary_image[x][y];
 }
 
 int getColourRGB(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned int x, unsigned int y) {
-    // Return 1 if pixel is white and 0 if the pixel is black
+    // Return 1 if pixel is white and 0 if the pixel is black.
     int sum = input_image[x][y][0] + input_image[x][y][1] + input_image[x][y][2];
     return sum > 382;
 }
 
 // Switch colour of pixel (x,y) between black and white.
 void switchColour(unsigned char binary_image[BMP_WIDTH][BMP_HEIGTH], unsigned int x, unsigned int y) {
-    // Switch between black and white based on current pixel
+    // Switch between black and white based on current pixel.
     if (getColour(binary_image, x, y)) {
         forceBlack(binary_image, x, y);
     } else {
@@ -41,6 +42,7 @@ void switchColour(unsigned char binary_image[BMP_WIDTH][BMP_HEIGTH], unsigned in
 }
 
 
+// Write a 2D list binary_image from the RGB bitmap input_image.
 void rgbToBinary (unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char binary_image[BMP_WIDTH][BMP_HEIGTH]) {
     for (int x = 0; x < BMP_WIDTH; x++) {
         for (int y = 0; y < BMP_HEIGTH; y++) {
@@ -54,25 +56,27 @@ void rgbToBinary (unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]
 }
 
 
+// Write an RGB bitmap input_image from the 2D list binary_image.
+// Note that this image will neccisarily be polarised, any pixel will be either completely black or completely white.
 void binaryToRGB (unsigned char binary_image[BMP_WIDTH][BMP_HEIGTH], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]){
-  for (int x = 0; x < BMP_WIDTH; x++)
-  {
-    for (int y = 0; y < BMP_HEIGTH; y++)
-    {
-      for (int c = 0; c < BMP_CHANNELS; c++)
-      {
+  for (int x = 0; x < BMP_WIDTH; x++) {
+    for (int y = 0; y < BMP_HEIGTH; y++) {
+      for (int c = 0; c < BMP_CHANNELS; c++) {
         output_image[x][y][c] = binary_image[x][y] * 255;
       }
     }
   }
 }
 
-//Declaring the array to store the image (unsigned char = unsigned 8 bit)
+// Declare the arrays to store the RGB images.
 unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
 unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
+
+// Declare the array to store the binary images.
 unsigned char binary_image[BMP_WIDTH][BMP_HEIGTH];
 
-//Main function
+
+// Main function.
 int main(int argc, char** argv)
 {
   //argc counts how may arguments are passed
@@ -89,10 +93,10 @@ int main(int argc, char** argv)
 
   printf("Example program - 02132 - A1\n");
 
-  //Load image from file
+  // Load image from file.
   read_bitmap(argv[1], input_image);
 
-  //Run grayscale
+  // Write the binary image.
   rgbToBinary(input_image, binary_image);
 
   binaryToRGB(binary_image, output_image);
